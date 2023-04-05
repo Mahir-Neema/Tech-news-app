a01f1b45bf61ceba98866d2b92e1a24ea1bbf543
Mahir-Neema <mohi10503@gmail.com>
2023-04-05 19:32:42 +0530

    added basic css file fir news article

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useGlobalContext } from './Context';
+import "./Articles.css"
 
 function Articles() {
     const {hits,nofPages,isLoading} = useGlobalContext();
@@ -13,7 +14,19 @@ function Articles() {
     return (<>
         <div>Tech News</div>
         {hits.map((post,index)=>{
-            return <h1>{post.title}</h1>
+            const {title,author,objectID, url, num_comments} = post;
+            return <>
+                <div className="card">
+                    <h2>{title}</h2>
+                    <p>
+                        By <span>{author}</span> || <span>{num_comments}</span> comments
+                    </p>
+                    <div className="card-button">
+                        <a href="url" className='url_button'>Read More..</a>
+                        <a href="#" className='remove_button'>Remove</a>
+                    </div>
+                </div>
+            </>
         })}
     </>
     )
